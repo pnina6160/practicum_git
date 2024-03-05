ec4e671ca25ece40f942a69a17f64cb69f347091
Pnina <pnina6160@gmail.com>
2024-03-05 22:34:49 +0200

    my first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i love you❤️");
+            Console.ReadLine();
+        }
+    }
+}
