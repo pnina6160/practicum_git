7e62ab8ffbd3984a5be6e593c4e22c5d1270c796
michalravid <152034941+michalravid@users.noreply.github.com>
2024-03-06 14:50:56 +0200

    Update Program.cs
    "commit on git"

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace les1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("i love me❤️????");
+            Console.WriteLine("i love me❤️❤️❤️❤️");
             Console.WriteLine("love you❤️?!");
            Console.WriteLine("I love you!!!!");
 
