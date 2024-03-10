bfe3377f071e6019585d669fbb3916f521ec7f3d
Gitty <gity2755@gmail.com>
2024-03-11 00:33:57 +0200

    try merge

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace les1
         {
 
 
-            Console.WriteLine("i love gitty❤️❤️❤️❤️");
+            Console.WriteLine("i love Pnina❤️❤️❤️❤️");
             Console.WriteLine("love you❤️?!");
            Console.WriteLine("I love you!!!!");
 
